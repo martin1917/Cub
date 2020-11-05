cfea45fb9777a69be4b76136b57090f3b1cff875
martin1917 <martin.levin02@mail.ru>
2020-11-05 13:35:37 +0300

    game

---
+++ Assets/isGool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class isGool : MonoBehaviour
+{
+    public GameObject sphere;
+    public Text info;
+    Vector3 start_pos;
+    float dis1, dis2, count = 0;
+    void Start()
+    {
+        start_pos = sphere.transform.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 curr_pos = sphere.transform.position;
+            dis1 = Vector3.Distance(curr_pos, start_pos);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 curr_pos = sphere.transform.position;
+            dis2 = Vector3.Distance(curr_pos, start_pos);
+
+            if (dis2 > dis1)
+            {
+                count += 1;
+                info.text = "Score: " + count.ToString();
+            }
+        }
+    }
+}
---
+++ Assets/tmp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    Vector3 targetPos;
+    public GameObject cube;
+    public float rotSpeed = 2.0f;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            targetPos = hit.point;
+        }
+
+        Vector3 dir = new Vector3(targetPos.x - cube.transform.position.x, 0, targetPos.z - cube.transform.position.z);//направление поворота
+        Quaternion quaternion = Quaternion.LookRotation(dir);//задаем поворот
+
+
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            cube.transform.rotation = Quaternion.Lerp(cube.transform.rotation, quaternion, rotSpeed * Time.deltaTime);
+        }
+    }
+}
